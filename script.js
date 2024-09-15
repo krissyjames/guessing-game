a900662d91affb7b965f049f4722278330edcc2f
krissyjames <kristina.ashley.cook@gmail.com>
2024-09-15 12:47:48 -0400

    Stretch goal 1 complete

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+//Stretch Goal 1
+//Allow for user name add
+
+let userName = prompt("What's your name?");
+
 //Round 1
 //User prompt to guess a number between 1-50
 //If the user is correct, alert you won and the number.
@@ -6,13 +11,13 @@
 let number = (Math.floor(Math.random() * 50)+1);
 console.log(number);
 let guesses = 0;
-let numberGuess = prompt("Guess a number between 1 and 50! You have 3 chances left.");
+let numberGuess = prompt(`Hey ${userName}! Guess a number between 1 and 50! You have 3 chances left.`);
 if (numberGuess == number) {
-    alert(`You have won! Congratulations! ${number} is the correct number.`);
+    alert(`You have won! Congratulations, ${userName}! ${number} is the correct number.`);
 } else if (numberGuess > number && guesses == 0) {
-    alert("Nope! That number is too high! Try again. You have 2 guesses remaining.", guesses++);
+    alert(`Nope! That number is too high! Try again, ${userName}. You have 2 guesses remaining.`, guesses++);
 } else if (numberGuess < number && guesses == 0) {
-    alert("Nope! That number is too low! Try again. You have 2 guesses remaining.", guesses++); 
+    alert(`Nope! That number is too low! Try again, ${userName}. You have 2 guesses remaining.`, guesses++); 
 }
 //Round 2
 //User prompt to guess the number again, with chances left decremented
@@ -23,11 +28,11 @@ if (numberGuess != number && guesses == 1) {
     numberGuess = prompt("Guess a number between 1 and 50! You have 2 chances left.");
 }
 if (numberGuess == number && guesses == 1) {
-    alert(`You did it! Congratulations! ${number} is the correct number.`);
+    alert(`You did it, ${userName}! Congratulations! ${number} is the correct number.`);
 } else if (numberGuess > number && guesses == 1) {
-    alert("Sorry! That number is too high! Try again. You have 1 more guess.", guesses++);
+    alert(`Sorry, ${userName}! That number is too high! Try again. You have 1 more guess.`, guesses++);
 } else if (numberGuess < number && guesses == 1) {
-    alert("Sorry! That number is too low! Try again. You have 1 more guess.", guesses++); 
+    alert(`Sorry, ${userName}! That number is too low! Try again. You have 1 more guess.`, guesses++); 
 }
 
 //Round 3
@@ -39,10 +44,10 @@ if (numberGuess != number && guesses == 2) {
     numberGuess = prompt("Guess a number between 1 and 50! If you get this wrong, you lose the game.");
 }
 if (numberGuess == number && guesses == 2) {
-    alert(`Whew, you did it! Congratulations! ${number} is the correct number.`);
+    alert(`Whew, you did it! Congratulations, ${userName}! ${number} is the correct number.`);
 } else if (numberGuess > number && guesses == 2) {
-    alert(`Sorry, that number was too high. You have lost the game. The correct number was ${number}.`);
+    alert(`Sorry, ${userName}, that number was too high. You have lost the game. The correct number was ${number}.`);
 } else if (numberGuess < number && guesses == 2) {
-    alert(`Sorry, that number was too low. You have lost the game. The correct number was ${number}.`);
+    alert(`Sorry, ${userName}, that number was too low. You have lost the game. The correct number was ${number}.`);
 }
 
